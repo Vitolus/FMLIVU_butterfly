607fc721f4ae8aebce7091ea974ba004ae238f5b
Vitolus <vito.davide00@gmail.com>
2024-06-25 19:55:47 +0200

    doing tests to understand things

--- main.py
+++ main.py
@@ -1,29 +1,23 @@
 #%%
-# Import libraries
+import mlcroissant as mlc
+import tensorflow_datasets as tfds
 import pandas as pd
 import matplotlib.pyplot as plt
 #%%
-# Defining data for the dataframe
-data = {
-    'Basket': ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P'],
-    'Apples': [10, 20, 30, 56, 40, 40, 67, 47, 40, 4, 49, 52, 5, 56, 35, 45],
-    'Bananas': [15, 6, 3, 45, 67, 44, 45, 11, 14, 18, 13, 12, 1, 34, 12, 12]
-}
-
-# Creating the dataframe
-df = pd.DataFrame(data)
-
-df
+# 1. Point to a local or remote Croissant file
+url = "https://huggingface.co/api/datasets/fashion_mnist/croissant"
+# 2. Inspect metadata
+print(mlc.Dataset(url).metadata.to_json())
 #%%
-# Calculate the sums
-sum_apples = df['Apples'].sum()
-sum_bananas = df['Bananas'].sum()
-
-# Create a bar chart
-plt.bar(['Apples', 'Bananas'], [sum_apples, sum_bananas], color=['red', 'blue'])
-
-# Set a title
-plt.title('Comparison of total Apples and Bananas')
-
-# Show the plot
-plt.show()
+# 3. Use Croissant dataset in your ML workload
+builder = tfds.dataset_builders.CroissantBuilder(jsonld=url, file_format='array_record')
+builder.download_and_prepare()
+#%%
+# 4. Split for training/testing
+train, test = builder.as_data_source(split=['default[:80%]', 'default[80%:]'])
+# 5. Display train and test
+# use pandas to display the data
+train_df = pd.DataFrame(train)
+test_df = pd.DataFrame(test)
+print(train_df.head())
+print(test_df.head())
