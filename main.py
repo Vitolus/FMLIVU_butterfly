bd601d5d3f9a6948f35ec846bbcd37e82bf575dd
Vitolus <vito.davide00@gmail.com>
2024-06-26 17:59:25 +0200

    add tutorial of pytorch. Partial completion

--- main.py
+++ main.py
@@ -1,23 +1,44 @@
-#%%
-import mlcroissant as mlc
-import tensorflow_datasets as tfds
-import pandas as pd
-import matplotlib.pyplot as plt
-#%%
-# 1. Point to a local or remote Croissant file
-url = "https://huggingface.co/api/datasets/fashion_mnist/croissant"
-# 2. Inspect metadata
-print(mlc.Dataset(url).metadata.to_json())
-#%%
-# 3. Use Croissant dataset in your ML workload
-builder = tfds.dataset_builders.CroissantBuilder(jsonld=url, file_format='array_record')
-builder.download_and_prepare()
-#%%
-# 4. Split for training/testing
-train, test = builder.as_data_source(split=['default[:80%]', 'default[80%:]'])
-# 5. Display train and test
-# use pandas to display the data
-train_df = pd.DataFrame(train)
-test_df = pd.DataFrame(test)
-print(train_df.head())
-print(test_df.head())
+import os
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+
+
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super(NeuralNetwork, self).__init__()
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(28 * 28, 512),  # input layer
+            nn.ReLU(),
+            nn.Linear(512, 512),  # hidden layer
+            nn.ReLU(),
+            nn.Linear(512, 10),  # output layer
+            nn.ReLU()
+        )
+
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
+
+
+if __name__ == '__main__':
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    print(f'Using {device} device')
+
+    model = NeuralNetwork().to(device)
+    print(model)
+
+    X = torch.rand(1, 28, 28, device=device)
+    logits = model(X)
+    pred_probab = nn.Softmax(dim=1)(logits)
+    y_pred = pred_probab.argmax(1)
+    print(f"Predicted class: {y_pred}")
+
+    print(f"First Linear weights: {model.linear_relu_stack[0].weight} \n")
+    print(f"First Linear biases: {model.linear_relu_stack[0].bias} \n")
+
+    for name, param in model.named_parameters():
+        print(f"Layer: {name} | Size: {param.size()} | Values : {param[:2]} \n")
