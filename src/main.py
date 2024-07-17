cf1ccc380225683b3ce9519b3668f50d206abb3e
Vitolus <vito.davide00@gmail.com>
2024-07-17 17:28:11 +0200

    fixing and refactoring

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 #%% import libraries
 import os
 import cv2
+from PIL import Image
 import atom
 import atom.data_cleaning as dc
 import atom.feature_engineering as fe
@@ -14,22 +15,23 @@ from torchinfo import summary
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-
+#%%
+path = 'data/images'
+pixels_per_side = 224
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 print("Using device: ", device)
 #%% building the dataset
 data = []
 labels = []
-path = 'data/images'
 folder = os.listdir(path)
 for file in folder:
     img = cv2.imread(str(os.path.join(path, file)))
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    img = cv2.resize(img, (224, 224))
+    img = cv2.resize(img, (pixels_per_side, pixels_per_side))
     data.append(img)
     labels.append(int(file[:3]) - 1)
 labels = np.array(labels)
-labels = labels.reshape(-1, 1)
+#labels = labels.reshape(-1, 1)
 label_dict = {0: 'Danaus plexippus',
               1: 'Heliconius charitonius',
               2: 'Heliconius erato',
@@ -40,13 +42,29 @@ label_dict = {0: 'Danaus plexippus',
               7: 'Pieris rapae',
               8: 'Vanessa atalanta',
               9: 'Vanessa cardui'}
+print([label_dict[labels[i]] for i in range(10)])
+#%%
+class MyDataset(torch.utils.data.Dataset):
+    def __init__(self, data, labels, transform=None):
+        self.data = data
+        self.labels = labels
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        data = self.data[idx]
+        data = Image.fromarray(data, mode='RGB')
+        if self.transform:
+            data = self.transform(data)
+        labels = torch.tensor(self.labels[idx], dtype=torch.long)
+        return data, labels
 #%%
 trans = transforms.Compose([
-    transforms.Resize(256),
-    transforms.CenterCrop(224),
     transforms.ToTensor()
 ])
-dataset = datasets.DatasetFolder(path, transform=trans, loader=lambda x: cv2.imread(x, cv2.COLOR_BGR2RGB))
+dataset = MyDataset(data, labels, transform=trans)
 dataloader = DataLoader(dataset, batch_size=64, shuffle=False)
 total_sum = torch.zeros(3)
 total_count = 0
@@ -63,46 +81,34 @@ std = [std[0].item(), std[1].item(), std[2].item()]
 print(mean, std)
 #%%
 trans = transforms.Compose([
-    transforms.Resize(256),
-    transforms.CenterCrop(224),
     transforms.ToTensor(),
     transforms.Normalize(mean=mean, std=std)
 ])
-dataset = datasets.ImageFolder('data/PetImages', transform=trans)
-dataloader = DataLoader(dataset, batch_size=64, shuffle=False)
+dataset = MyDataset(data, labels, transform=trans)
+dataloader = torch.utils.data.DataLoader(dataset, batch_size=64, shuffle=False)
 data = []
 for img, _ in dataloader:
     data.append(img.numpy())
-data = np.concatenate(data, axis=0)
+data = np.concatenate(data, axis=0).transpose((0, 2, 3, 1))
 #%%
 print(data.shape, labels.shape)
 print(data)
 print(labels)
 #%%
-clf = atom.ATOMClassifier(data, y=labels,
-                          test_size=0.2,
-                          device='cpu',
-                          engine='sklearn',
-                          n_jobs=-1, verbose=2, random_state=1)
+data = data.reshape(len(data), -1)
 #%% Data cleaning
 data, labels = (dc.Pruner(strategy=['lof', 'iforest'],
                           device='cpu',
                           engine='sklearn',
                           verbose=2,
-                          iforest={'n_estimators': 100,
-                                   'contamination': 'auto',
-                                   'bootstrap': True,
-                                   'n_jobs': -1, 'verbose': 2, 'random_state': 1},
-                          lof={'n_neighbors': 20,
-                               'metric': 'minkowski',
-                               'contamination': 'auto',
-                               'n_jobs': -1}
+                          iforest={'contamination': 'auto', 'bootstrap': True, 'n_jobs': -1, 'random_state': 1},
+                          lof={'n_neighbors': 20, 'contamination': 'auto', 'n_jobs': -1}
                           )
                 .fit_transform(data, labels))
 #%%
-
-
-
+data = data.values.reshape(-1, pixels_per_side, pixels_per_side, 3)
+labels = labels.values
+cat_labels = F.one_hot(torch.tensor(labels, requires_grad=False), num_classes=10).numpy()
 #%%
 # TODO: AFTER DIVISION BETWEEN TRAIN AND VALIDATION. TRY DIFFERENT STRATS TO DETERMINE THE BEST ONE
 strats = ['ADASYN','BorderlineSMOTE','SVMSMOTE','KMeansSmote']
@@ -110,6 +116,3 @@ data, labels = (dc.Balancer(strategy=strats[0],
                             n_jobs=-1,verbose=2,random_state=1,
                             )
                 .fit_transform(data, labels))
-#%%
-data = data.reshape(len(data), 224, 224, 3)
-cat_labels = F.one_hot(torch.tensor(labels, requires_grad=False), num_classes=10).numpy()
