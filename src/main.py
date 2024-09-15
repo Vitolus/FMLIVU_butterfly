318c2a90ec5bc53d0edec99b1d5042a248e4b87e
Vitolus <vito.davide00@gmail.com>
2024-09-15 20:09:06 +0200

    tweaks

--- src/main.py
+++ src/main.py
@@ -222,18 +222,18 @@ def objective(trial, trainset, X, y):
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 10, 5)
+        self.conv1 = nn.Conv2d(3, 16, 3, 1)
         self.pool = nn.MaxPool2d(2)
-        self.conv2 = nn.Conv2d(10, 25, 5)
-        self.conv3 = nn.Conv2d(25, 16, 5)
+        self.conv2 = nn.Conv2d(16, 32, 11, 1)
+        self.conv3 = nn.Conv2d(32, 64, 5, 1)
         self.flat = nn.Flatten()
-        self.fc1 = nn.Linear(16 * 18 * 18, 64) # ((100 -4)/2 -4)/2 -4
-        self.fc2 = nn.Linear(64, 10)
+        self.fc1 = nn.Linear(64 * 7 * 7, 128)
+        self.fc2 = nn.Linear(128, 10)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
-        x = F.relu(self.conv3(x))
+        x = self.pool(F.relu(self.conv3(x)))
         x = self.flat(x)
         x = F.dropout(F.relu(self.fc1(x)), p=0.5)
         x = self.fc2(x)
